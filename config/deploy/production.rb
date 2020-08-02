1db1469f8cb0da0465edabfabd1e81060b3bb837
vagrant <vagrant@localhost.localdomain>
2020-08-02 14:05:08 +0000

    add EIP

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '18.181.96.152', user: 'ec2-user', roles: %w{app web db}
+server '54.150.120.71', user: 'ec2-user', roles: %w{app web db}
 
 
 # role-based syntax
