1951fa0e9b1a764e6f82092971a4891e39c45943
vagrant <vagrant@localhost.localdomain>
2020-08-05 08:52:44 +0000

    delete ssl-web2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 server '54.150.120.71', user: 'ec2-user', roles: %w{app web db}
-server '18.183.125.111', user: 'ec2-user', roles: %w{app web db}
+#server '18.183.125.111', user: 'ec2-user', roles: %w{app web db}
 
 # role-based syntax
 # ==================
